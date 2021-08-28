0ed350fe5d9d92f5d2d49480ccea13ec5e2c6770
dongchijoseph <huyblogging@gmail.com>
2021-08-28 17:36:38 +0700

    add API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,18 +1,28 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    zipcode == '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 puts 'EventManager initialized.'
 
-contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+  legislators = legislators.officials
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
