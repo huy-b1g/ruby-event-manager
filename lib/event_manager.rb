ce268744e9496f1e8b6056250a3fcd15dc76125b
dongchijoseph <huyblogging@gmail.com>
2021-08-29 15:26:01 +0700

    add form letter template

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,11 +3,27 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+    legislators_names = legislators.map(&:name)
+    legislators_names.join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -18,19 +34,7 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislators_names = legislators.map(&:name)
-    legislators_string = legislators_names.join(', ')
-  rescue StandardError
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-  end
+  legislators_string = legislators_by_zipcode(zipcode)
 
   puts "#{name} #{zipcode} #{legislators_string}"
 end
